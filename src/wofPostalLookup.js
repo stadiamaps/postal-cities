0ff3592cc1cd0175bb1f1c03d51f93f6f4a74d7c
Ian Wagner <ian.wagner@stadiamaps.com>
2023-07-04 19:48:03 +0900

    Preload cache instead of waiting to do lookups

--- src/wofPostalLookup.js
+++ src/wofPostalLookup.js
@@ -4,14 +4,8 @@ const db = new Sqlite3(postalDbFile);
 
 var cache = {};
 
-function getPostalLocalities(postalcode) {
-  // TODO: Filter by location?
-  if (cache[postalcode]) {
-    return cache[postalcode];
-  }
-
-  const sql = `
-        SELECT g.body
+const sql = `
+        SELECT g.body, name
         FROM geojson g
         INNER JOIN spr s ON g.id = s.id
         WHERE
@@ -19,32 +13,33 @@ function getPostalLocalities(postalcode) {
             AND g.body != ''
             AND s.parent_id > 1
             AND s.is_current = 1
-            AND s.placetype = 'postalcode'
-            AND s.name = ?;
+            AND s.placetype = 'postalcode';
     `;
 
-  const rows = db.prepare(sql).all(postalcode);
+const rows = db.prepare(sql).all();
 
-  const results = [];
-  for (const row of rows) {
-    const geojson = JSON.parse(row.body);
+for (const row of rows) {
+  const geojson = JSON.parse(row.body);
 
-    if ('properties' in geojson && 'mz:postal_locality' in geojson.properties &&
-      'geom:latitude' in geojson.properties && 'geom:longitude' in geojson.properties) {
-      const lat = geojson.properties['geom:latitude'];
-      const lon = geojson.properties['geom:longitude'];
-      const postal_locality = geojson.properties['mz:postal_locality'];
-      const postal_locality_alt = geojson.properties['mz:postal_locality_alt'] || [];
+  if ('properties' in geojson && 'mz:postal_locality' in geojson.properties &&
+    'geom:latitude' in geojson.properties && 'geom:longitude' in geojson.properties) {
+    const lat = geojson.properties['geom:latitude'];
+    const lon = geojson.properties['geom:longitude'];
+    const postal_locality = geojson.properties['mz:postal_locality'];
+    const postal_locality_alt = geojson.properties['mz:postal_locality_alt'] || [];
 
-      results.push({
-        lat, lon, postal_locality, postal_locality_alt
-      });
+    if (!(row.name in cache)) {
+      cache[row.name] = [];
     }
+
+    cache[row.name].push({
+      lat, lon, postal_locality, postal_locality_alt
+    });
   }
+}
 
-  cache[postalcode] = results;
-
-  return results;
+function getPostalLocalities(postalcode) {
+    return cache[postalcode] || [];
 }
 
 module.exports = getPostalLocalities;
